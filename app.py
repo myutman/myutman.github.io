86820b69123099bc8dd2bcdf7cc73492bf4f7021
Mikhail Yutman <myutman@gmail.com>
2024-07-27 04:34:52 +0200

    Fixing host

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import sys
 import justpy as jp
 from src.algorithm.vizualization import make_table
 
-jp.justpy(make_table, host="localhost", start_server=False)
+jp.justpy(make_table, host="0.0.0.0", start_server=False)
 server = jp.app
