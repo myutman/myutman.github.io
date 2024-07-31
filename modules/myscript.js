2ac147ca4b07c0c40e8f429594bce04270877e72
Mikhail Yutman <myutman@gmail.com>
2024-07-31 15:12:00 +0200

    Added events storing in IndexedDB

--- modules/myscript.js
+++ modules/myscript.js
@@ -12,6 +12,16 @@ function Coord(x, y) {
     this.y = y;
 }
 
+function getUTCDateMilliseconds() {
+    let date = new Date();
+    return date.toISOString();
+}
+
+function getUTCDate() {
+    let date = new Date();
+    return date.toLocaleDateString("en-US")
+}
+
 function clickHandler(ev) {
     posX = ev.offsetX;
     posY = ev.offsetY;
@@ -29,14 +39,15 @@ function clickHandler(ev) {
             field_color[Y][X] %= 2;
             drawCell(ctx, X, Y);
         }
+        putEventToStorage(
+            {
+                x: X,
+                y: Y,
+                date: getUTCDateMilliseconds()
+            }
+        )
         console.log(`X=${X}, Y=${Y}`)
     }
-}
-
-
-function getUTCDate() {
-    let date = new Date();
-    return date.toUTCString();
 }
 
 
@@ -49,6 +60,18 @@ function putStateToStorage(state) {
         const db = event.target.result;
         const objectStore = db.transaction("levels", "readwrite").objectStore("levels");
         objectStore.add(state);
+    };
+}
+
+function putEventToStorage(click_event) {
+    const request = indexedDB.open("MyDB", 3);
+    request.onerror = (event) => {
+        console.log(event.type)
+    };
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        const objectStore = db.transaction("events", "readwrite").objectStore("events");
+        objectStore.add(click_event);
     };
 }
 
@@ -76,7 +99,7 @@ function drawCell(ctx, x, y) {
 }
 
 
-function drawFullCanvas(field) {
+function drawFullCanvas() {
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
     for (let i = 0; i < N; i++) {
@@ -90,6 +113,67 @@ function drawFullCanvas(field) {
 
 var state;
 
+
+function upgradeDB(event) {
+    const db = event.target.result;
+
+    objectStore = db.createObjectStore("levels", { keyPath: "date" });
+    objectStore.onerror = (event) => {
+        console.log(event.type);
+    }
+
+    objectStore1 = db.createObjectStore("events", { keyPath: "date" });
+    objectStore1.onerror = (event) => {
+        console.log(event.type);
+    } 
+}
+
+var events_loaded;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+var events = new Array();
+
+async function loadEvents(event) {
+    const cursor = event.target.result;
+    if (cursor) {
+        click_event = cursor.value;
+        if (click_event === undefined) {
+            return;
+        }
+        console.log(click_event);
+
+        events.push(click_event);
+
+        cursor.continue();
+        return;
+    } else {
+        console.log("Events loaded");
+    }
+
+    if (events.length > 0) {
+        while (field_set === undefined) {
+            await delay(1);
+        }
+        console.log("After while");
+
+        for (let i = 0; i < events.length; i++) {
+            let click_event = events[i];
+            if (field[click_event.y][click_event.x] > 0) {
+                field_color[click_event.y][click_event.x] += 1;
+                field_color[click_event.y][click_event.x] %= 2;
+            } else {
+                field_color[click_event.y][click_event.x] += 1;
+                field_color[click_event.y][click_event.x] %= 3;
+            }
+        }
+    }
+    events_loaded = true;
+    console.log("All events been handled");
+}
+
+var field_set;
+
 async function loadState(event) {
     const cursor = event.target.result;
     if (cursor) {
@@ -101,7 +185,8 @@ async function loadState(event) {
             return;
         }
     }
-    if (state === undefined) {
+    if (state === undefined || state.date != getUTCDate()) {
+        console.log("Getting field");
         field = await genField(N);
         putStateToStorage(
             {
@@ -119,13 +204,19 @@ async function loadState(event) {
             field_color[i][j] = 0;
         }
     }
+    field_set = true;
     console.log(field);
-    drawFullCanvas(field);
+    while (events_loaded === undefined) {
+        await delay(1);
+    }
+    drawFullCanvas();
 }
 
+const BACKEND_URL = "http://backend.com:5432";
+// const BACKEND_URL = "https://cave-puzzle-3f21cce9636b.herokuapp.com";
 
 async function genField(N) {
-    const field = await fetch("https://cave-puzzle-3f21cce9636b.herokuapp.com/gen-cave", {
+    const field = await fetch(BACKEND_URL + `/gen-cave?field_size=${N}`, {
         method: "GET",
         headers: {
             "Content-type": "applications/json"
@@ -139,18 +230,22 @@ async function genField(N) {
 
 
 function kek() {
+    let canvas_height = N * cellSize + 2 * offsetY;
+    let canvas_width = N * cellSize + 2 * offsetX;
+    let canvas_placement = document.getElementById("canvas_placement");
+    let canvas = document.createElement("canvas");
+    canvas.height = `${canvas_height}`;
+    canvas.width = `${canvas_width}`;
+    canvas.id = "canvas";
+    canvas_placement.appendChild(canvas);
+
+    console.log(state);
+
     const request = indexedDB.open("MyDB", 3);
     request.onerror = (event) => {
         console.log(event.type)
     };
-    request.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        objectStore = db.createObjectStore("levels", { keyPath: "date"});
-        objectStore.onerror = (event) => {
-            console.log(event.type);
-        }
-    };
-
+    request.onupgradeneeded = upgradeDB
 
     request.onsuccess = (event) => {
         const db = event.target.result;
@@ -162,6 +257,13 @@ function kek() {
             console.log(event.type);
         }
         openCursor.onsuccess = loadState;
+
+        var objectStore1 = db.transaction("events", "readwrite").objectStore("events");
+        const openCursor1 = objectStore1.openCursor(null, "prev");
+        openCursor1.onerror = (event) => {
+            console.log(event.type);
+        }
+        openCursor1.onsuccess = loadEvents;
     };
 }
 
